e9bbc99308acd62469019f9a6f563dbc604c9545
FIEF-Nohell <noel.hmnn@gmail.com>
2023-02-24 20:50:42 +0100

    bit to image converter working

--- encoder.py
+++ encoder.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import math
 import os
 
 def file_to_bitstring(filename):
@@ -11,14 +12,12 @@ def file_to_bitstring(filename):
             byte = file.read(1)
     return bit_string
 
+# Read file and convert to bit string
+
 input_filename = "elo.txt"
 output_folder = "./output"
-bitstring_filename = os.path.join(output_folder, "bits.txt")
-width = 1920  # set the width of the image
-height = len(bit_string) // width  # calculate the height of the image
-
-# Read file and convert to bit string
 bit_string = file_to_bitstring(input_filename)
+bitstring_filename = os.path.join(output_folder, "bits.txt")
 
 # Save bit string to file
 with open(bitstring_filename, "w") as file:
@@ -27,6 +26,11 @@ with open(bitstring_filename, "w") as file:
 # Read bit string from file and rebuild original file
 with open(bitstring_filename, "r") as file:
     bit_string = file.read()
+
+root = math.ceil(math.sqrt(len(bit_string)))
+
+width = root 
+height = root
 
 image_data = []
 for i in range(len(bit_string)):
@@ -38,6 +42,6 @@ for i in range(len(bit_string)):
 # add a red pixel at the end of the image to indicate the end of the bit string
 image_data.append((255, 0, 0))
 
-image = Image.new("RGB", (width, height+1))  # increase the height by 1 for the red pixel
+image = Image.new("RGB", (width, height))  # increase the height by 1 for the red pixel
 image.putdata(image_data)
 image.show()
