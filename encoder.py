5d79d99e7536a70b8a53b0023817e640ded03573
FIEF-nohell <noel.hmnn@gmail.com>
2023-02-24 23:33:10 +0100

    cleanup

--- encoder.py
+++ encoder.py
@@ -1,53 +1,51 @@
-# Imports
 from PIL import Image
+import bitstring
 import math
 import os
-# ---------------------------------------
 
+filename = "image.png"
+filetype = os.path.splitext(filename)[1]
+output_filename = "./output/" + f"output{filetype}"
 
+with open(filename, "rb") as file:
+    binary_data = file.read()
 
-def file_to_bitstring(filename, buffer_size=1024):
-    with open(filename, "rb") as file:
-        bit_string = ""
-        while True:
-            buffer = file.read(buffer_size)
-            if not buffer:
-                break
-            for byte in buffer:
-                bits = bin(byte)[2:].rjust(8, "0")
-                bit_string += bits
-    return bit_string
+bitstring = bitstring.BitArray(filename=filename).bin
+print(bitstring)
 
-
-# Define input file and output folder
-input_filename = "pp.txt"
-output_folder = "./output"
-
-# Read file and convert to bit string
-bit_string = file_to_bitstring(input_filename)
-bitstring_filename = os.path.join(output_folder, "bits.txt")
-
-# Save bit string to file
-with open(bitstring_filename, "w") as file:
-    file.write(bit_string)
+with open(output_filename, "wb") as file:
+    file.write(binary_data)
 
 # calculate the image dimensions
-root = math.ceil(math.sqrt(len(bit_string)))
+root = math.ceil(math.sqrt(len(bitstring)))
 width = root 
 height = root
 # ---------------------------------------
-
-
 
 # create the image, display it and save it
 image_data = []
 counter = 1
 total = root*root
-for i in range(len(bit_string)):
+for i in range(len(bitstring)):
     if i % root == 0: 
         print("row done: " + str(counter) + "/" + str(root) + " | " + str(root*counter) + "/" + str(total)+ " pixels")
         counter += 1
-    if bit_string[i] == "0":
+    if bitstring[i] == "0":
+        image_data.append((0, 0, 0))  # black pixel
+    else:
+        image_data.append((255, 255, 255))  # white pixel
+
+# add a red pixel at the end of the image to indicate the end of the bit string
+image_data.append((169, 00, 00))
+
+extension_bitstring = ''.join(format(ord(c), '08b') for c in filetype)
+print("ext: " + str(extension_bitstring))
+
+for i in range(len(extension_bitstring)):
+    if i % root == 0: 
+        print("row done: " + str(counter) + "/" + str(root) + " | " + str(root*counter) + "/" + str(total)+ " pixels")
+        counter += 1
+    if extension_bitstring[i] == "0":
         image_data.append((0, 0, 0))  # black pixel
     else:
         image_data.append((255, 255, 255))  # white pixel
