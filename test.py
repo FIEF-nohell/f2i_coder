549c7b0e84fec97e8cf00a1db046ce6ed6098095
FIEF-nohell <noel.hmnn@gmail.com>
2023-02-24 23:31:33 +0100

    wtf shit works

--- test.py
+++ test.py
@@ -38,6 +38,21 @@ for i in range(len(bitstring)):
 # add a red pixel at the end of the image to indicate the end of the bit string
 image_data.append((169, 00, 00))
 
+extension_bitstring = ''.join(format(ord(c), '08b') for c in filetype)
+print("ext: " + str(extension_bitstring))
+
+for i in range(len(extension_bitstring)):
+    if i % root == 0: 
+        print("row done: " + str(counter) + "/" + str(root) + " | " + str(root*counter) + "/" + str(total)+ " pixels")
+        counter += 1
+    if extension_bitstring[i] == "0":
+        image_data.append((0, 0, 0))  # black pixel
+    else:
+        image_data.append((255, 255, 255))  # white pixel
+
+# add a red pixel at the end of the image to indicate the end of the bit string
+image_data.append((169, 00, 00))
+
 image = Image.new("RGB", (width, height))
 image.putdata(image_data)
 image.save("./output/image.png")
