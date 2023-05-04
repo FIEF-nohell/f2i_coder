16c2296924b3c9a1fdd35a7ea7ecb4fb750f432d
Administrator <zivi@plan-g.at>
2023-05-04 12:56:51 +0200

    improved decoder speed by about 1200%

--- decoder.py
+++ decoder.py
@@ -1,7 +1,5 @@
 # imports
-import bitstring as bs
 from PIL import Image
-from tqdm import tqdm
 import numpy as np
 import time
 import os
@@ -12,10 +10,9 @@ output_folder = "./decoded/"
 filename = os.listdir(input_folder)[0]
 
 # load the image and get the pixels
+Image.MAX_IMAGE_PIXELS = None
 image = Image.open(input_folder + filename)
-#pix = image.load()
 image_dimensions = image.size[0]
-#colorset = [255, 169, 0]
 print("\n\n---- Image dimensions: " + str(image_dimensions) + "x" + str(image_dimensions) + " ----\n")
 
 # Convert the image to a NumPy array
@@ -27,28 +24,23 @@ pixel_array = pixel_array.reshape(num_pixels, 3)
 
 # Extract the red values into array
 r_values = pixel_array[:, 0]
-r_values[r_values == 255] = 1
 
 # Find the index of the number 169 in the array
 index = np.where(r_values == 169)[0][0]
 
-binary_data = r_values[0:index]
+binary_data = r_values[0:index].astype(np.uint8)
 binary_data_ext = r_values[index+1:]
+binary_data_ext[binary_data_ext == 255] = 1
 
 index2 = np.where(binary_data_ext == 169)[0][0]
 binary_data_ext = binary_data_ext[:index2]
 
 print(f"Converting into bytes...\n")
-t1 = time.time()
 
 # Convert the array to a string representing a file extension
 extension = ''.join([chr(int(''.join(map(str, binary_data_ext[i:i+8])), 2)) for i in range(0, len(binary_data_ext), 8)])
 
-result = bytearray(binary_data.tobytes())
-print(result[0:30])
-
-t2 = round(time.time() - t1, 2)
-print(f"Converting binary format took {t2} seconds\n")
+result = bytes(np.packbits(binary_data))
 
 t1 = time.time()
 with open(output_folder + "decoded" + extension, "wb") as file:
