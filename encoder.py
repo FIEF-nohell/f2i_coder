3924005266bd93df97ee11b747fbbeba1c105683
Administrator <zivi@plan-g.at>
2023-05-09 11:40:38 +0200

    encode multiple files

--- encoder.py
+++ encoder.py
@@ -2,6 +2,8 @@
 from PIL import Image
 import bitstring
 import threading
+import random
+import string
 import time
 import math
 import sys
@@ -26,81 +28,90 @@ if didnt_exist:
     sys.exit()
 print(f"\nLoading file to memory. Depending on the file size this can take a while.\n")
 try:
-    filename = os.listdir(input_folder)[0]
+    test = os.listdir(input_folder)[0]
+    files = os.listdir(input_folder)
 except:
     print(f"\nNo input file found. Please relaunch the script with a file in the {input_folder} folder.\n")
     sys.exit()
-filetype = os.path.splitext(filename)[1]
-file_size = os.path.getsize(input_folder+filename) / (1024 * 1024)
+for file in files:
+    filetype = os.path.splitext(file)[1]
+    file_size = os.path.getsize(input_folder+file) / (1024 * 1024)
 
-bitstring = bitstring.BitArray(filename=input_folder + filename).bin
+    print(f"file: {file}")
 
-# calculate the image dimensions
-extension_bitstring = ''.join(format(ord(c), '08b') for c in filename)
-root = math.ceil(math.sqrt(len(bitstring)+len(extension_bitstring)))
-width, height = root, root
-image_data = [None] * len(bitstring)
-total_pixels = height*width
+    bitstring_from_file = bitstring.BitArray()
+    bitstring_from_file = bitstring.BitArray(filename=(input_folder + file)).bin
 
-print(f"\n---- Calculating {root}x{root} image with {total_pixels} pixels | Input size: {file_size:.2f} MB | Estimated output size: {file_size * 2.2:.2f} MB ----\n")
-# record the start time
-start_time = time.time() 
-num_iterations = len(bitstring)
+    # calculate the image dimensions
+    extension_bitstring = ''.join(format(ord(c), '08b') for c in file)
+    root = math.ceil(math.sqrt(len(bitstring_from_file)+len(extension_bitstring)))
+    width, height = root, root
+    image_data = [None] * len(bitstring_from_file)
+    total_pixels = height*width
 
-# function to process a range of bits
-def process_bits(bitstring, start, end, image_data, thread_index):
-    for i in range(start, end):
-        if bitstring[i] == "0":
-            image_data[i] = (0, 0, 0)  # black pixel
+    print(f"\n---- Calculating {root}x{root} image with {total_pixels} pixels | Input size: {file_size:.2f} MB | Estimated output size: {file_size * 2.2:.2f} MB ----\n")
+    # record the start time
+    start_time = time.time() 
+    num_iterations = len(bitstring_from_file)
+
+    # function to process a range of bits
+    def process_bits(bitstring, start, end, image_data, thread_index):
+        for i in range(start, end):
+            if bitstring[i] == "0":
+                image_data[i] = (0, 0, 0)  # black pixel
+            else:
+                image_data[i] = (255, 255, 255)
+        end_time = time.time() - start_time
+        print(f"Thread finished. {end_time:.2f} seconds elapsed. (Thread ID: {thread_index})", end="\r")
+
+    # create four threads and assign equal work to each
+    num_threads = 32
+    chunk_size = num_iterations // num_threads
+    threads = []
+
+    print(f"Launching {num_threads} threads.")
+
+    for i in range(num_threads):
+        start = i * chunk_size
+        end = (i+1) * chunk_size if i != num_threads - 1 else num_iterations
+        thread = threading.Thread(target=process_bits, args=(bitstring_from_file, start, end, image_data, i))
+        thread.start()
+        threads.append(thread)
+
+    # wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+    # add a different pixel at the end of the image to indicate the end of the bit string
+    image_data.append((169, 0, 0))
+
+    for i in range(len(extension_bitstring)):
+        if extension_bitstring[i] == "0":
+            image_data.append((0, 0, 0)) 
         else:
-            image_data[i] = (255, 255, 255)
-    end_time = time.time() - start_time
-    print(f"Thread finished. {end_time:.2f} seconds elapsed. (Thread ID: {thread_index})", end="\r")
+            image_data.append((255, 255, 255))
 
-# create four threads and assign equal work to each
-num_threads = 32
-chunk_size = num_iterations // num_threads
-threads = []
+    # add a different pixel at the end of the image to indicate the end of the bit string
+    image_data.append((169, 00, 00))
 
-print(f"Launching {num_threads} threads.")
+    t1 = time.time()
 
-for i in range(num_threads):
-    start = i * chunk_size
-    end = (i+1) * chunk_size if i != num_threads - 1 else num_iterations
-    thread = threading.Thread(target=process_bits, args=(bitstring, start, end, image_data, i))
-    thread.start()
-    threads.append(thread)
+    image = Image.new("RGB", (width, height))
+    print("\nWriting data to image.")
+    image.putdata(image_data)
 
-# wait for all threads to finish
-for thread in threads:
-    thread.join()
+    print("Saving image.")
+    image_format = "png"
+    chars = string.ascii_letters + string.digits
+    random_string = ''.join(random.choices(chars, k=6))
+    output_filename = output_folder + random_string + "encoded." + image_format.lower()
+    image.save(output_filename, format=image_format)
 
-# add a different pixel at the end of the image to indicate the end of the bit string
-image_data.append((169, 0, 0))
+    t2 = round(time.time() - t1, 2)
 
-for i in range(len(extension_bitstring)):
-    if extension_bitstring[i] == "0":
-        image_data.append((0, 0, 0)) 
-    else:
-        image_data.append((255, 255, 255))
+    image_size = os.path.getsize(output_filename) / (1024 * 1024)
+    ratio = image_size / file_size * 100
 
-# add a different pixel at the end of the image to indicate the end of the bit string
-image_data.append((169, 00, 00))
+    print(f"\n---- Image created in {t2} seconds | Output file size: {image_size:.4f} MB (+{ratio:.2f}%) ----\n")
 
-t1 = time.time()
-
-image = Image.new("RGB", (width, height))
-print("\nWriting data to image.")
-image.putdata(image_data)
-
-print("Saving image.")
-image_format = "png"
-output_filename = output_folder + "encoded." + image_format.lower()
-image.save(output_filename, format=image_format)
-
-t2 = round(time.time() - t1, 2)
-
-image_size = os.path.getsize(output_filename) / (1024 * 1024)
-ratio = image_size / file_size * 100
-
-print(f"\n---- Image created in {t2} seconds | Output file size: {image_size:.4f} MB (+{ratio:.2f}%) ----\n")
+print("All files have been encoded.\n\n")
